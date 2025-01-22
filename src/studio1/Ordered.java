ae8bbd5f32b31ea30cab8452ca92de1606ac3d76
gracewang <gracewang@10.232.214.247>
2025-01-22 14:00:33 -0600

    finished

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,9 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+	boolean isOrdered = ((x>y && x> z)&& y>z)|| ((z> y&& z>x)&& y> x);
+	System.out.println(isOrdered);
 	}
 
 }
